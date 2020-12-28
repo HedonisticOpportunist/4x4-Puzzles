5744c46ee273ee2fbc2b814f8d0ab8791b47cd9d
HedonisticOpportunist <fujitahibaraki@gmail.com>
2020-12-28 19:50:49 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import math
 import random
 from queue import Queue
 from numpy import zeros
@@ -45,11 +44,17 @@ def permute_rows(vector_input, x, y, z):
 
 
 def linear_search(list_to_search, item):
-    for element in range(len(list_to_search)):
-        if element == item:
+    for element in range(0, len(list_to_search)):
+        if list_to_search[element] == item:
             return True
-
     return False
+
+
+def contains_integers(array_with_numbers):
+    if sum(array_with_numbers) == 10:
+        return True
+    else:
+        return False
 
 
 def check_column(vector_puzzle, j):
@@ -58,15 +63,16 @@ def check_column(vector_puzzle, j):
         return 'Invalid number. Choose a number 1 and 3'
     else:
         temp = list()
-        found = 0
-        for i in range(4):
+        # create an empty vector store all integers of that temp vector
+        found = list()
+        for i in range(len(vector_puzzle)):
             temp.append(vector_puzzle[i][j])
 
-        for item in range(4):
-            if linear_search(temp, j):
-                found += 1
+        for k in temp:
+            if linear_search(temp, k):
+                found.append(k)
 
-        if found == 4:
+        if contains_integers(found):
             return True
         else:
             return False
@@ -83,14 +89,18 @@ def check_col(input_puzzle_vector):
 def create_col_from_grids(vector_as_puzzle):
     if (len(vector_as_puzzle)) >= 0 or vector_as_puzzle is not None:
         # create a temporary two dimensional vector of type int and length 4
-        temp = zeros([4, 4], int)
-        for i in range(4):
-            row = math.floor(i / 2)
-            temp[i] = vector_as_puzzle[i][row]
-            for j in range(4):
-                col = j % 2
-                temp[j] = vector_as_puzzle[j][col]
+        # declare the rows
+        rows_upper = vector_as_puzzle[0]
+        rows_lower = vector_as_puzzle[1]
 
+        # declare the list
+        temp = zeros([4, 4], int)
+
+        # declare the columns
+        temp[0] = rows_upper[0]
+        temp[1] = rows_upper[1]
+        temp[2] = rows_lower[0]
+        temp[3] = rows_lower[1]
         return temp
     return 'Please enter a valid vector'
 
@@ -111,34 +121,128 @@ def make_solution(vector_row):
         columns = create_col_from_grids(puzzle_input)
         if check_col(columns):
             return puzzle_input
-        return False
+        else:
+            return False
 
 
 def make_blanks(vector_to_make_blank, n):
-    if n > len(vector_to_make_blank) or n < 0:
-        return "Please enter a valid number, which is either smaller than " \
-               "the length of the vector or bigger than zero."
-    for item in range(n):
-        # generate a number between 0 and 3
-        random_number_row = random.randint(0, 3)
-        random_number_col = random.randint(0, 3)
+    if isinstance(vector_to_make_blank, list):
+        if n > len(vector_to_make_blank) or n < 0:
+            return "Please enter a valid number, which is either smaller than " \
+                   "the length of the vector or bigger than zero."
+        for item in range(n):
+            # generate number between 1 and 3
+            random_number_row = random.randint(0, 3)
+            random_number_col = random.randint(0, 3)
 
-        # replace the selected row and column with None
-        vector_to_make_blank[random_number_row][random_number_col] = None
-    return vector_to_make_blank
+            # replace the selected row and column with None
+            vector_to_make_blank[random_number_row][random_number_col] = None
+            return vector_to_make_blank
+    else:
+        return 'This vector is not a proper array.'
 
 
 def print_array(vector):
-    for i in range(len(vector)):
-        for j in range(len(vector[i])):
-            print(vector[i][j], end=' ')
-        print()
+    if isinstance(vector, list):
+        for i in range(len(vector)):
+            for j in range(len(vector[i])):
+                print(vector[i][j], end=' ')
+            print()
+    else:
+        print('This vector is not a valid puzzle.')
 
 
-# test
-output = [2, 3, 4, 1]
-solution = make_solution(output)
-#print_array(solution)
+# switch rows
+def switch_rows(row, i, j):
+    temp_row = row[i]
+    row[i] = row[j]
+    row[j] = temp_row
+    return row
 
+
+def make_solution_with_changing_rows(vector_row):
+    # make the vector
+    puzzle_input = make_vector(vector_row)
+
+    # swap rows
+    puzzle_input = switch_rows(puzzle_input, random.randint(0, 4), random.randint(0, 4))
+    # cyclic permutations
+    cyclic_permutation_x = [2, 1]
+    cyclic_permutation_y = [1, 2]
+    cyclic_permutation_z = [3, 3]
+    for i in range(len(cyclic_permutation_x)):
+        puzzle_input = permute_rows(puzzle_input,
+                                    cyclic_permutation_x[i],
+                                    cyclic_permutation_y[i],
+                                    cyclic_permutation_z[i]
+                                    )
+        columns = create_col_from_grids(puzzle_input)
+        if check_col(columns):
+            return puzzle_input
+        else:
+            return False
+
+
+# tests
+valid_output = [1, 2, 3, 4]
+solution = make_solution(valid_output)
 blank_vector = make_blanks(solution, 1)
 print_array(blank_vector)
+
+print()
+print('**********')
+print()
+
+valid_output_two = [2, 3, 4, 1]
+solution = make_solution(valid_output_two)
+blank_vector = make_blanks(solution, 2)
+print_array(blank_vector)
+
+print()
+print('**********')
+print()
+
+valid_output_three = [3, 4, 1, 2]
+solution = make_solution(valid_output_three)
+blank_vector = make_blanks(solution, 3)
+print_array(blank_vector)
+
+print()
+print('**********')
+print()
+
+valid_output_four = [4, 1, 2, 3]
+solution = make_solution(valid_output_four)
+blank_vector = make_blanks(solution, 2)
+print_array(blank_vector)
+
+print()
+print('**********')
+print()
+
+invalid_output = [4, 4, 4, 3]
+solution = make_solution(invalid_output)
+blank_vector = make_blanks(solution, 1)
+print_array(blank_vector)
+
+print()
+print('**********')
+print()
+
+invalid_output_two = [1, 1, 1, 3]
+solution = make_solution(invalid_output_two)
+blank_vector = make_blanks(solution, 1)
+print_array(blank_vector)
+
+print()
+print('**********')
+print()
+
+last_output = [1, 2, 3, 4]
+solution = make_solution_with_changing_rows(last_output)
+blank_vector = make_blanks(solution, 1)
+print_array(blank_vector)
+
+print()
+print('******** AND DONE **********')
+print()
